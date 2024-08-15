348de4b9566de4d54d12913979b1e7d3123edce1
unknown <dasha.nebogina@mail.ru>
2024-08-15 11:55:48 +0300

    feat: add abs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 module.exports = function reverse (n) {
-  let array = Array.from(String(n));
+  let array = Array.from(String(Math.abs(n)));
   for (let i = (array.length - 1); i > 0; i--) {
     if (array[i] == '0') {
         array.pop();
     }
     break;
 }
-  return array;
+  return array.reverse().join('');
 }
